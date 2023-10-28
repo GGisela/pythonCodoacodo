ab0b768432e31665033c1545c18123951494a9bc
Gisela G <gomezgisela70@gmail.com>
2023-10-28 02:05:05 -0300

    subi

--- js/page.js
+++ js/page.js
@@ -3,3 +3,4 @@ const body = document.querySelector('body')
 fetch('https://pixabay.com/api/?key={ KEY }&q=yellow+flowers&image_type=photo')
 .then(result => result.json())
 .then(data => console.log(data) )
+
